154f967d77336dbbd43b988790b2706be8df2901
magic <maciej.macierzynski@gmail.com>
2013-03-02 16:15:09 +0000

    changed to default

--- eUp/App_Start/RouteConfig.cs
+++ eUp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace eUp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "UserTables", action = "ListTable", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
